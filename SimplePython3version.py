23b49c049a9410c039e4a8a175775a0e5e3657e4
Elisabeth Reid <16.elisabethw@gmail.com>
2023-09-12 11:54:14 -0400

    File Cleaning and Repaire
    Main now uses SimplePython3Version.py for the agent.
    
    Removed other files
    
    Placed python_actr module into file.
    
    Updated dependencies to correct ones.

--- SimplePython3version.py
+++ SimplePython3version.py
@@ -1,7 +1,9 @@
-
-
-
-
+import sys
+import python_actr
+from EmilyMotorModule import *
+from RTModule import *
+from python_actr.actr import *
+from random import randrange, uniform
 
 
 class MyAgent(ACTR):
@@ -42,7 +44,7 @@ class MyAgent(ACTR):
     def START_AK(b_context='status:starting_game planning_unit:none',
                  b_unit_task='unit_task:START state:running',
                  b_method='state:finished',
-                 b_visual='AK'):  
+                 b_visual='AK'):
         b_plan_unit.modify(planning_unit='AK',unit_task='AK',state='begin_sequence',type='ordered')
 
         b_context.modify(status='occupied')
@@ -53,7 +55,7 @@ class MyAgent(ACTR):
     def START_RP(b_context='status:starting_game planning_unit:none',
                  b_unit_task='unit_task:START state:running',
                  b_method='state:finished',
-                 b_visual='RP'):  
+                 b_visual='RP'):
         b_plan_unit.modify(planning_unit='RP',unit_task='RP',state='begin_sequence',type='ordered')
         b_context.modify(status='occupied')
         print ('run_RP_PU')
@@ -63,7 +65,7 @@ class MyAgent(ACTR):
     def START_HW(b_context='status:starting_game planning_unit:none',
                  b_unit_task='unit_task:START state:running',
                  b_method='state:finished',
-                 b_visual='HW'):  
+                 b_visual='HW'):
         b_plan_unit.modify(planning_unit='HW',unit_task='HW',state='begin_sequence',type='ordered')
         b_plan_unit_order.set('counter:one first:HW second:RP third:AK fourth:finished') ######## new buffer
         b_context.modify(status='occupied')
@@ -133,7 +135,7 @@ class MyAgent(ACTR):
         target='responce'
         content='AK-WM-1432'
         motor.enter_response(target, content)
-        
+
     def AK_SU(b_unit_task='unit_task:AK state:running',
               vision_finst='state:finished',
               focus='WM'):
@@ -194,7 +196,7 @@ class MyAgent(ACTR):
         motor.enter_response(target, content)
 
     ### Unkown code
-        
+
     def RP_identify2(b_unit_task='unit_task:RP state:running',
                      vision_finst='state:finished',
                      focus='SU'):
@@ -202,7 +204,7 @@ class MyAgent(ACTR):
         choices = ['YP','ZB']
         x=random.choice(choices)
         motor.referee_action('display', 'state', x)
-        ############################### referee        
+        ############################### referee
         motor.see_code()
         focus.set('code_seen')
         print ('waiting to see if YP or ZB')
@@ -276,7 +278,7 @@ class MyAgent(ACTR):
         motor.enter_response(target, content)
 
     ### Unkown code
-        
+
     def HW_identify3(b_unit_task='unit_task:HW state:running',
                      vision_finst='state:finished',
                      focus='YP'):
@@ -284,14 +286,14 @@ class MyAgent(ACTR):
         choices = ['FJ','SU','ZB']
         x=random.choice(choices)
         motor.referee_action('display', 'state', x)
-        ############################### referee        
+        ############################### referee
         motor.see_code()
         focus.set('code_seen')
         print ('waiting to see if FJ, SU, or ZB')
-        
+
 
     #### FJ or SU or ZB then end
-        
+
     def HW_FJ(b_unit_task='unit_task:HW state:running',
               vision_finst='state:finished',
               focus='code_seen',
